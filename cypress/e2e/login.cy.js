1151b2ed5e5e5a51ce334418c867ab9bc53395af
camilaqueirozleite <ernanicamila@hotmail.com>
2026-04-14 09:51:01 +0100

    Adicionei dados provenientes do fixture e alterei o .gitignone

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,29 +1,37 @@
 describe('Login', () => {
   beforeEach(() => {
-    //arrange
+    // arrange
     cy.visit('http://localhost:4000')
     cy.screenshot('apos-visitar-pagina')
   })
-  it.only('Login com dados válidos deve permitir entrada no sistema', () => {
-    
-    //act
-    cy.get('#username').click().type("julio.lima")
-    cy.get('#senha').click().type("123456")
+
+  it('Login com dados válidos deve permitir entrada no sistema', () => {
+    // act
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.valida.usuario)
+      cy.get('#senha').click().type(credenciais.valida.senha)
+    })
+
     cy.screenshot('apos-preencher-dados-validos')
+
     cy.contains('button', 'Entrar').click()
+
     cy.screenshot('apos-clicar-no-botao-entrar')
 
-    //assert
-    cy.contains('h4' , 'Realizar Transferência').should('be.visible')
+    // assert
+    cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
   it('Login com dados inválidos devem apresentar mensagem de erro', () => {
-       //act
-    cy.get('#username').click().type("julio.lima")
-    cy.get('#senha').click().type("123458")
+    // act
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.invalida.usuario)
+      cy.get('#senha').click().type(credenciais.invalida.senha)
+    })
+
     cy.contains('button', 'Entrar').click()
 
-    //assert
-   cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+    // assert
+    cy.get('.toast').should('contain', 'Erro no login. Tente novamente.')
   })
 })
